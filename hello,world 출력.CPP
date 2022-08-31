851ff124f42db1dc47ef4158e8979980daaca606
sommizzu <someee4399@gmail.com>
2022-08-31 11:00:00 +0900

    hello,world출력

---
+++ hello,world 출력.CPP
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, world!" << std::endl;
+    return EXIT_SUCCESS;
+}
